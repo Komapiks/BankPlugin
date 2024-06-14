a9a41f3f97d8d171e23db8e398874801293a0555
Komapiks <nata.anishchenko3331999@gmail.com>
2024-06-14 17:41:45 +0700

    Add files via upload

---
+++ src/main/java/ru/komap/bankplugin/meteorite/MeteorCreator.java
@@ -0,0 +1,13 @@
+package ru.komap.bankplugin.meteorite;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
+public class MeteorCreator {
+    private void createBigMeteorite(Location location) {
+        World world = location.getWorld();
+
+    }
+}
