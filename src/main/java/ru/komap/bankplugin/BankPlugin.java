2c03c2a2ae7502dc470e053216791b84f2200c6d
Komapiks <nata.anishchenko3331999@gmail.com>
2025-10-05 23:26:14 +0700

    relocate utils to another plugin

--- src/main/java/ru/komap/bankplugin/BankPlugin.java
+++ src/main/java/ru/komap/bankplugin/BankPlugin.java
@@ -2,16 +2,12 @@ package ru.komap.bankplugin;
 
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
-import ru.komap.bankplugin.discord.DiscordBot;
-import ru.komap.bankplugin.utils.*;
 
 import java.util.*;
 import java.util.logging.Logger;
 
 public final class BankPlugin extends JavaPlugin implements CommandExecutor {
     public Logger logger = this.getLogger();
-    private MotdManager motdManager;
-    private DiscordBot discordBot;
 
     @Override
     public void onEnable() {
@@ -20,36 +16,13 @@ public final class BankPlugin extends JavaPlugin implements CommandExecutor {
         Objects.requireNonNull(this.getCommand("withdraw")).setExecutor((CommandExecutor) new BankCommandExecutor(this));
         Objects.requireNonNull(this.getCommand("transfer")).setExecutor((CommandExecutor) new BankCommandExecutor(this));
         Objects.requireNonNull(this.getCommand("balance")).setExecutor((CommandExecutor) new BankCommandExecutor(this));
-        Objects.requireNonNull(this.getCommand("me")).setExecutor(new Me());
-        Objects.requireNonNull(this.getCommand("try")).setExecutor(new Try());
-        this.getServer().getPluginManager().registerEvents(new EventsListener(), this);
-        Objects.requireNonNull(this.getCommand("disaster")).setExecutor(new Disaster());
-        Objects.requireNonNull(getCommand("minusfps")).setExecutor(new MinusFps());
-        ExplodeArrow listener = new ExplodeArrow(this);
 
-        // Регистрируем команду и обработчик событий
-        getCommand("giveExplodingArrow").setExecutor(listener);
-        getServer().getPluginManager().registerEvents(listener, this);
 
-        motdManager = new MotdManager(getDataFolder());
-        Objects.requireNonNull(this.getCommand("togglelink")).setExecutor(motdManager);
 
-        discordBot = new DiscordBot();
-        try {
-            String discordToken = "MTE0MzYxODczNDU5MDc5OTkwMg.GBIzS1.tBnYdkqK-3bGaC2aCq49szLwCvuBtPJl4xpI_c"; // Укажите ваш токен
-            discordBot.startBot(discordToken);
-            getLogger().info("Discord бот успешно запущен.");
-        } catch (Exception e) {
-            getLogger().severe("Ошибка при запуске Discord бота: " + e.getMessage());
-        }
     }
 
     @Override
     public void onDisable() {
         logger.info("Bank is stopped.");
-        if (discordBot != null) {
-            discordBot.stopBot();
-            getLogger().info("Discord бот остановлен.");
-        }
     }
 }
--- src/main/java/ru/komap/bankplugin/discord/commands/Command.java
+++
@@ -1,7 +0,0 @@
-package ru.komap.bankplugin.discord.commands;
-
-import discord4j.core.event.domain.message.MessageCreateEvent;
-
-public interface Command {
-    void execute(MessageCreateEvent event, String[] args);
-}
